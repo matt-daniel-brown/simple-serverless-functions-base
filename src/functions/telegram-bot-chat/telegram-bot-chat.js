b8e47fa2518a6e39445bd82fee2b4ab263f74b73
Matt Brown <hi@mattbrown.email>
2020-01-03 08:41:43 -0600

    Updated telegram-bot-chat.js

--- src/functions/telegram-bot-chat/telegram-bot-chat.js
+++ src/functions/telegram-bot-chat/telegram-bot-chat.js
@@ -1,38 +1,53 @@
 
-const TelegramBot = require('node-telegram-bot-api');
-const token = '1022774009:AAEm0YwjwRaXV-NoqrQrexv8_yQ6S00XO-Y';
+const telegram = require('telegram-bot-api');
+const botToken = '1022774009:AAEm0YwjwRaXV-NoqrQrexv8_yQ6S00XO-Y';
 
-exports.handler = async (event, context) => {
-    try {
-        // return { statusCode: 200, body: `Hello from a serverless function!` };
-        // Create a bot that uses 'polling' to fetch new updates
-        const bot = new TelegramBot(token, {polling: true});
 
-        // Matches "/echo [whatever]"
-        bot.onText(/\/echo (.+)/, (msg, match) => {
-            // 'msg' is the received Message from Telegram
-            // 'match' is the result of executing the regexp above on the text content
-            // of the message
-
-            const chatId = msg.chat.id;
-            const resp = match[1]; // the captured "whatever"
-
-            // send back the matched "whatever" to the chat
-            bot.sendMessage(chatId, resp);
-        });
-
-        // Listen for any kind of message. There are different kinds of
-        // messages.
-        bot.on('message', (msg) => {
-            const chatId = msg.chat.id;
-
-            // send a message to the chat acknowledging receipt of their message
-            bot.sendMessage(chatId, 'Received your message');
-        });
-    } catch (err) {
-        return { statusCode: 500, body: err.toString() };
+const api = new telegram({
+    token: botToken,
+    updates: {
+        enabled: true
     }
-};
+});
+
+api.on('message', function(message)
+{
+    // Received text message
+    console.log(message);
+    api.sendMessage(`YOU SAID : ${message}`);
+});
+
+api.on('inline.query', function(message)
+{
+    // Received inline query
+    console.log(message);
+});
+
+api.on('inline.result', function(message)
+{
+    // Received chosen inline result
+    console.log(message);
+});
+
+api.on('inline.callback.query', function(message)
+{
+    // New incoming callback query
+    console.log(message);
+});
+
+api.on('edited.message', function(message)
+{
+    // Message that was edited
+    console.log(message);
+});
+
+api.on('update', function(message)
+{
+    // Generic update object
+    // Subscribe on it in case if you want to handle all possible
+    // event types in one callback
+    console.log(message);
+});
 
 
 
