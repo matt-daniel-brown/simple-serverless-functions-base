deaf4763ba953896375e8aa64a4a7509d4ca61d9
Matt Brown <hi@mattbrown.email>
2020-01-03 08:46:58 -0600

    re-commit and push after build

--- src/functions/telegram-bot-chat/telegram-bot-chat.js
+++ src/functions/telegram-bot-chat/telegram-bot-chat.js
@@ -1,6 +1,6 @@
 
 
-const fetch = require("node-fetch");
+
 exports.handler = async (event, context) => {
     try {
         /*return { statusCode: 200, body: `Hello from a serverless function!` };*/
